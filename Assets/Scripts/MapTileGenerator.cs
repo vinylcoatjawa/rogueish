2dc2238dcd28ac34c818f60a81d3860010d57cab
Jelci <andras3141592@gmail.com>
2022-04-18 23:41:05 +0200

    Refined room gen

--- Assets/Scripts/MapTileGenerator.cs
+++ Assets/Scripts/MapTileGenerator.cs
@@ -1,13 +1,173 @@
 using UnityEngine;
 using TMPro;
 using System;
+using System.Collections.Generic;
 
-public class MapTileGenerator
+
+
+
+public class LevelGenerator
+{ 
+    
+    private int levelWidth, levelHeight, seed, roomSideLength, roomX, roomZ;
+    public List<MapTile> mapTiles = new List<MapTile>(); // tiles will be collected into this to enable easy loop through
+    private HashSet<Vector2> rooms = new HashSet<Vector2>();
+    public LevelGenerator(int levelWidth, int levelHeight, int seed, int roomSideLength)
+    {
+        this.levelWidth = levelWidth;
+        this.levelHeight = levelHeight;
+        this.seed = seed;
+        this.roomSideLength = roomSideLength;
+    }
+
+    public void CreateFloorTiles()
+    {
+        MapTile mapTile;
+
+        for (int x = -this.levelWidth; x < this.levelWidth; x++)
+        {
+            for (int z = -this.levelHeight; z < this.levelHeight; z++)
+            {
+
+                float cubeDistFromOrigo = MathF.Sqrt(x * x + z * z) / 150; // the farther away the cube is the less likely for it to be walkable
+                bool isWall = GetPerlinDepth(x, z, this.seed) > (.68f - cubeDistFromOrigo);
+                Vector3 tilePostition = new Vector3(x, isWall ? 1 :0, z); //raise up the ones having isWall flag
+                mapTile = new MapTile(tilePostition);
+
+                mapTile.isWall = isWall;
+                mapTiles.Add(mapTile);
+            }
+        }
+    }
+    private float GetPerlinDepth(int x, int z, int seed)
+    {
+        float xNoise = (float)x * Mathf.PI + seed;
+        float zNoise = (float)z * Mathf.PI + seed;
+        return Mathf.PerlinNoise(xNoise, zNoise);
+    }
+
+    public void AssignRooms()
+    {
+        
+        foreach (var mapTile in mapTiles)
+        {
+            float tileXCoord = mapTile.tilePosition.x;
+            float tileZCoord = mapTile.tilePosition.z;
+
+            tileXCoord += this.levelWidth;
+            tileZCoord += this.levelHeight;
+
+            int roomXCoord = (int)Mathf.Floor(tileXCoord / this.roomSideLength);
+            int roomZCoord = (int)Mathf.Floor(tileZCoord / this.roomSideLength);
+
+            mapTile.roomNumber = new Vector2(roomXCoord, roomZCoord); // room named
+            rooms.Add(mapTile.roomNumber); // room enlisted
+        }
+    }
+
+
+
+    public void GetOpenings()
+    {
+        foreach (var mapTile in mapTiles)
+        {
+            MapTile east = GetTileByPosition(mapTile.tilePosition + new Vector3(1, 0, 0));
+            MapTile north = GetTileByPosition(mapTile.tilePosition + new Vector3(0, 0, 1));
+
+            if (north != null && mapTile.roomNumber != north.roomNumber && !mapTile.isWall|| east != null && mapTile.roomNumber != east.roomNumber && !mapTile.isWall) 
+            {
+                mapTile.isOnRoomBorder = true;
+            }
+        }
+    }
+    public MapTile GetTileByPosition(Vector3 position)
+    {
+        MapTile theTile;
+
+        foreach (var i in mapTiles)
+        {
+            if (i.tilePosition == position)
+            {
+                theTile = i;
+                return theTile;
+            }
+            
+        }
+        return null;
+    }
+
+    public void RemoveBadRooms()
+    {
+        GetFloorWallProportionPerRoom();
+        
+        
+    }
+
+    private void GetFloorWallProportionPerRoom()
+    {
+        int numberOfFloors;
+        int numberOfWalls;
+        float proportion;
+        foreach (var room in rooms)
+        {
+            numberOfFloors = 0;
+            numberOfWalls = 0;
+            proportion = 0;
+            foreach (var mapTile in mapTiles)
+            {
+                if (mapTile.isWall && mapTile.roomNumber == room) { numberOfWalls++; } else if (!mapTile.isWall && mapTile.roomNumber == room) numberOfFloors++;
+            }
+            proportion = (float)numberOfWalls / numberOfFloors;
+            foreach (var mapTile in mapTiles)
+            {
+                if (proportion > 1f && mapTile.roomNumber == room && mapTile.tilePosition.y == 0)
+                {
+                    mapTile.tilePosition.y += 1;
+                }
+            }
+        }
+    }
+}
+
+
+
+public class MapTile
 {
-    private int tileHeigth, tileWidth;
+    public Vector3 tilePosition;
+    public Vector2 roomNumber;
+    public bool isWall;
+    public bool isOnRoomBorder;
+    public bool badRoom;
+    public MapTile(Vector3 tilePosition)
+    {
+        this.tilePosition = tilePosition;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*public class MapTileGenerator
+{
+    private int tileHeigth, tileWidth, roomX, roomZ;
     private float cellSize, gridOffset;
     private Vector3 originPosition;
-    //int[,] cells;
 
 
     public MapTileGenerator(int width, int heigth, float cellSize, Vector3 originPosition)
@@ -16,51 +176,86 @@ public class MapTileGenerator
         this.tileHeigth = heigth;
         this.cellSize = cellSize;
         this.originPosition = originPosition;
-        gridOffset = cellSize - 1 / cellSize;
+        gridOffset = cellSize * .95f;
         int roomSize = 10;
 
-        //cells = new int[width, heigth];
+
+
+        
 
         for (int x = -width; x < width + 1; x++)
         {
             for (int z = -heigth; z < heigth + 1; z++)
             {
                 int blockYValue;
-                if (GetPerlinDepth(x, z, 0.66f) > .6f) { blockYValue = 1; } else blockYValue = 0; // needs logic to smooth this, also add logic to make it harsher the farther we are from origin
-                Vector3 tilePosition = GetWorldCoords(x, blockYValue, z);
-                Vector3 tileRotation = new Vector3(90, 0, 0);
+                //if ( -1 < x  ) Debug.Log("if i: " + x + " and j: " + z + " then the noise is : " + GetPerlinDepth(x, z, 0.66f));
+                if (GetPerlinDepth(x, z, 159654) > .5f) { blockYValue = 1; } else blockYValue = 0; // needs logic to smooth this, also add logic to make it harsher the farther we are from origin
+                Vector3 tilePosition = GetWorldCoords(x , blockYValue, z) + new Vector3(cellSize / 2, 0, cellSize / 2);
                 GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                tile.name = "Cube at " + "(" + x + ", " + z + " )";
                 tile.transform.localScale = new Vector3(gridOffset, gridOffset, gridOffset);
                 tile.transform.position = tilePosition;
-                tile.transform.eulerAngles = tileRotation;
-
-                Debug.Log("if i: " + x + " and j: " + z + " then the noise is : " + GetPerlinDepth(x, z, 0.66f));
+                roomX = ((x + width) / roomSize);
+                roomZ = ((z + heigth) / roomSize);
+                Debug.Log("Tile: (" + x + ", " + z + ") is in room " + "(" + roomX + ", " + roomZ + ")");
             }
         }
 
-        for (int x = -width; x < width; x += roomSize)
+        for (int x = -(width + roomSize / 2); x < width; x += roomSize)
         {
-            for (int z = -heigth; z < heigth; z += roomSize)
+            for (int z = -(heigth + roomSize / 2); z < heigth; z += roomSize)
             {
-                Vector3 tilePosition = GetWorldCoords(x, 5, z);
-                Vector3 tileRotation = new Vector3(90, 0, 0);
-                GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                tile.transform.localScale = new Vector3(gridOffset, gridOffset, gridOffset);
-                tile.transform.position = tilePosition;
-                tile.transform.eulerAngles = tileRotation;
-                tile.GetComponent<Renderer>().material.color = Color.red;
-            }
-        }
+                // gets to middle of the room
+                
+
+                GameObject northWall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                northWall.name = "North Wall";
+                northWall.transform.localScale = new Vector3(roomSize, 0.01f, 3) * cellSize;
+                Vector3 northWallPos = GetWorldCoords(x, 0, z + roomSize / 2);
+                Vector3 northWallRot = new Vector3(90, 0, 0);
+                northWall.transform.position = northWallPos;
+                northWall.transform.eulerAngles = northWallRot;
+                northWall.GetComponent<Renderer>().material.color = Color.blue;
+
+                //Debug.Log(x + " and " + z);
+                //Vector3 northWallPos = GetWorldCoords(x, 5, z + roomSize / 2);
+                //Vector3 northWallRot = new Vector3(0, 0, 90);
+                //GameObject northWall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                //northWall.transform.position = northWallPos;
+                //northWall.transform.eulerAngles = northWallRot;
+                //northWall.GetComponent<Renderer>().material.color = Color.blue;
+
+                //GameObject middle = new GameObject("Middle of: (" + x + ", " + z + " )");
+                //middle.transform.position = roomMiddle;
+
+
+                
+                //Vector3 tilePosition = GetWorldCoords(x, 5, z);
+                //Vector3 tileRotation = new Vector3(90, 0, 0);
+                //GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                //tile.transform.localScale = new Vector3(gridOffset, gridOffset, gridOffset);
+                //tile.transform.position = tilePosition;
+                //tile.transform.eulerAngles = tileRotation;
+                //tile.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            } break;
+        } 
 
 
     }
 
-    private float GetPerlinDepth(int x, int z, float detailScale)
+    private float GetPerlinDepth(int x, int z, int seed)
     {
-        float xNoise = x / detailScale;
-        float zNoise = z / detailScale;
+        
+        float xNoise = (float)x * Mathf.PI + seed;
+        float zNoise = (float)z * Mathf.PI + seed;
+        Debug.Log("For (" + x + ", " + z + ") we get the noise input x: " + xNoise + " z: " + zNoise + " and perlin noise for these is " + Mathf.PerlinNoise(xNoise, zNoise));
+
+
         return Mathf.PerlinNoise(xNoise, zNoise);
     }
+
+
 
     private Vector3 GetWorldCoords(int x, int y, int z)
     {
@@ -73,27 +268,6 @@ public class MapTileGenerator
         z = Mathf.FloorToInt((worldPosition - originPosition).z / cellSize);
     }
 
-    public void GenerateWalls(int width, int height, int roomSize, Vector3 originPosition)
-    {
-        int levelWidth = 2 * width + 1;
-        int levelHeight = 2 * height + 1;
+    private void 
 
-        for (int x = -width; x < width; x += roomSize)
-        {
-            for (int z = -height; z < height; z += roomSize)
-            {
-                Vector3 tilePosition = GetWorldCoords(x, 5, z);
-                Vector3 tileRotation = new Vector3(90, 0, 0);
-                GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                tile.transform.localScale = new Vector3(gridOffset, gridOffset, gridOffset);
-                tile.transform.position = tilePosition;
-                tile.transform.eulerAngles = tileRotation;
-                tile.GetComponent<Renderer>().material.color = Color.red;
-            }
-        }
-
-
-
-    }
-
-}
+}*/
--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -1,14 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
+
 public class test : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //MapTileGenerator map;
+    LevelGenerator level;
+    List<MapTile> mapTiles = new List<MapTile>();
     void Start()
     {
-        MapTileGenerator map = new MapTileGenerator(50, 50, 10, Vector3.zero);
-        
+        level = new LevelGenerator(40, 40, 1695, 9);
+        level.CreateFloorTiles();
+        level.AssignRooms();
+        level.GetOpenings();
+        level.RemoveBadRooms();
+        ShowLog(level.mapTiles);
+
     }
 
+    private void ShowLog(List<MapTile> tilelist)
+    {
+        
+        foreach (var maptile in tilelist)
+        {
+
+            GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            tile.name = "Tile on: (" + maptile.tilePosition.x + ", " + maptile.tilePosition.z + " )";
+            tile.transform.position = maptile.tilePosition;
+            //Debug.Log(maptile.TilePosition + " is " + maptile.isWall);
+            //tile.GetComponent<Renderer>().material.color = new Color(maptile.RoomNumber.x / 10, 0, maptile.RoomNumber.y / 10, 1);
+            //if(maptile.TilePosition.y == 0) { tile.GetComponent<Renderer>().material.color = Color.gray; } else tile.GetComponent<Renderer>().material.color = Color.black;
+            if(maptile.isOnRoomBorder && tile.transform.position.y == 0) { 
+                tile.GetComponent<Renderer>().material.color = Color.red;
+                tile.transform.position = new Vector3(tile.transform.position.x, 0, tile.transform.position.z);
+            }
+            else if (maptile.isOnRoomBorder && tile.transform.position.y != 0) { tile.GetComponent<Renderer>().material.color = Color.red; }
+            //tile.GetComponent<Renderer>().material.color = new Color(maptile.RoomNumber.x / 10, 0, maptile.RoomNumber.y / 10, 1);
+            //Debug.Log("Tile on: (" + maptile.TilePosition.x + ", " + maptile.TilePosition.z + " ) has room numbers: " + maptile.RoomNumber.x + ", " + maptile.RoomNumber.y + " )");
+
+        }
+
+
+
+    }
 }
