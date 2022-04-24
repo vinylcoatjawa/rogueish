c68154ee54d26f03f7efac8fe90614610cab81d0
Jelci <andras3141592@gmail.com>
2022-04-24 12:24:23 +0200

    Cleared out some old scripts started with noise based mapgen

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using NoiseUtils;
+//using System;
 
 public class LevelGenerator : MonoBehaviour
 {
@@ -13,10 +14,132 @@ public class LevelGenerator : MonoBehaviour
 
     // 5. spice up room geometri with noise, probably Perlin noise
 
+    /* TERMINOLOGY:
+            - Corridor: straight bit of path
+    */
+    //public uint seed = 12890;
+    Noise noise = new Noise();
+    GameObject map;
+    int[,] mapGrid;
+    int gridMinX, gridMinZ, gridMaxX, gridMaxZ;
+
+    private void Awake()
+    {
+        map = new GameObject("The Map");
+        mapGrid = new int[100, 100];
+
+    }
+
+    private void LayPathBranch(Vector3 pathStartPos, uint minCorridorCount, uint maxCorridorCountDeviation, uint seed)  // parameters may be superflous but this function may be used for sidebranches as well
+    {
+        uint startX = (uint)pathStartPos.x;
+        uint startZ = (uint)pathStartPos.z;
+        Vector3 corridorStartPos = pathStartPos;
+        uint numberOfCorridors = minCorridorCount + (noise.Get2DNoiseUint(startX, startZ, seed) % maxCorridorCountDeviation); // calculate number of straight corridors to include in the path
+        for (int corridor = 0; corridor < numberOfCorridors; corridor++) // looping through the number of corridors to create them
+        {
+            Vector3 direction = ChooseDirection(corridorStartPos, corridor, seed);
+            //Debug.Log("Number of corridors: " + numberOfCorridors + " we are currently on corridor nr: " + (corridor + 1) + " and the chose direstion is: " + direction);
+            //Debug.Log(direction);
+            //Vector3 direction = new Vector3(1, 0, 0);
+            LayCorridor(corridorStartPos, direction, corridor, seed);
+            
+        }
+        
+    }
+
+    private Vector3 ChooseDirection(Vector3 corridorStartPos, int corridor, uint seed)
+    {
+        uint startX = (uint)corridorStartPos.x; 
+        uint startZ = (uint)corridorStartPos.z;
+        //Vector3 nextTile = corridorStartPos;
+
+        
+        uint probe = noise.Get1DNoiseUint((uint)corridor, seed); // we need to adjust the direction calculation in each while loop try
+        int directionX = 0; // setting initial direction
+        int directionZ = 0;
+
+        while (directionX == directionZ)
+        {
+            uint xComponent = startX + (uint)corridor * (uint)corridor + (uint)probe;
+            uint zComponent = startZ + (uint)corridor * (uint)corridor + ((uint)probe * 2);
+        
+            directionX = (int)noise.ZeroOrOne(xComponent, seed);
+            directionZ = (int)noise.ZeroOrOne(zComponent, seed);
+
+            probe += probe;
+        }
+        return new Vector3(directionX, 0, directionZ);
+
+    }
+
+    private void LayCorridor(Vector3 corridorStartPos, Vector3 corridorDirection, int corridor, uint seed)
+    {
+        Vector3 nextTile = corridorStartPos;
+        int startX = (int)nextTile.x;
+        int startZ = (int)nextTile.z;
+        int directionX = (int)corridorDirection.x;
+        int directionZ = (int)corridorDirection.z;
+        uint numberOfCorridorTiles = 2 + (noise.Get2DNoiseUint((uint)startX + (uint)corridor * (uint)corridor, (uint)startZ + (uint)corridor * (uint)corridor, seed) % 8); // minsteps is 2 so we at least make 2 steps and 8 + 2 at most
+        Debug.Log("Handling corridr: " + corridor + " which will have " + numberOfCorridorTiles + " floor tiles");
+        for (int step = 0; step < numberOfCorridorTiles; step++)
+        {
+            int gridX = startX + step * directionX;
+            int gridZ = startZ + step * directionZ;
+            mapGrid[gridX, gridZ] = 0;
+            if (gridX > gridMaxX) { gridMaxX = gridX; }
+            if (gridX < gridMinX) { gridMinX = gridX; }
+            if (gridZ > gridMaxZ) { gridMaxZ = gridZ; }
+            if (gridZ < gridMinZ) { gridMinZ = gridZ; }
+            Debug.Log(gridMaxX + " " + gridMinX + " " + gridMaxZ + " " + gridMinZ + " value " + mapGrid[gridX, gridZ]);
+        }
+        //corridorStartPos = nextTile + numberOfCorridorTiles * new Vector3(directionX, 0, directionZ); 
+
+    }
+
+
+    private int[,] WorldPosToMapgrid(Vector3 worldPosition)
+    {
+        int x = (int)worldPosition.x;
+        int z = (int)worldPosition.z;
+        int[,] res = new int[x, z];
+        return res;
+    }
+
+
+    void OnDrawGizmos()
+    {
+        if (mapGrid != null)
+        {
+            for (int x = gridMinX; x < gridMaxX; x++)
+            {
+                for (int y = gridMinZ; y < gridMaxZ; y++)
+                {
+                    Gizmos.color = (mapGrid[x, y] == 1) ? Color.black : Color.blue;
+                    Vector3 pos = new Vector3(x, 0, y);
+                    Gizmos.DrawCube(pos, Vector3.one);
+                }
+            }
+        }
+
+
+
+
+
+
+    }
+
+    private void Start()
+    {
+        LayPathBranch(Vector3.zero, 5, 5, (uint)Random.Range(1, uint.MaxValue));
+        //ChooseDirection(Vector3.zero, 6);
+    }
+
+
 
 
     /* returns a list of integers(x) to be used as x length straight corridor segments of a path */
-    private List<int> CorridorLister(int numberOfCorridors, int maxCorridorLength)
+    /*private List<int> CorridorLister(int numberOfCorridors, int maxCorridorLength)
     {
         List<int> stepList = new List<int>();
         for (int i = 0; i < numberOfCorridors; i++)
@@ -25,9 +148,9 @@ public class LevelGenerator : MonoBehaviour
             stepList.Add(curr);
         }
         return stepList;
-    }
+    }*/
     /* standard list popper, takes list pops last element */
-    private int ListPop(List<int> list)
+    /*private int ListPop(List<int> list)
     {
         int popped;
         int listLength = list.Count;
@@ -38,9 +161,9 @@ public class LevelGenerator : MonoBehaviour
             return popped;
         }
         else return 0;
-    }
+    }*/
     /* returns a unit vector pointing north or west*/
-    private Vector3 RandomCardinalDirection()
+    /*private Vector3 RandomCardinalDirection()
     {
         while (true)
         {
@@ -56,7 +179,7 @@ public class LevelGenerator : MonoBehaviour
                 return new Vector3(x_abs, 0, z_abs);
             }
         }
-    }
+    }*/
     /* returns a unit vector pointing south or east MAY NOT BE NEEDED*/
     /*private Vector3 SouthOrEast()
     {
@@ -76,7 +199,7 @@ public class LevelGenerator : MonoBehaviour
         }
     }*/
     /* lays floor tiles for a straight corridor section */
-    private void LayCorridorTiles(Vector3 startPos, Vector3 direction, int steps, GameObject map, Color tileColor)
+    /*private void LayCorridorTiles(Vector3 startPos, Vector3 direction, int steps, GameObject map, Color tileColor)
     {
         for (int i = 0; i < steps; i++)
         {
@@ -90,9 +213,9 @@ public class LevelGenerator : MonoBehaviour
             tile.tag = "Floor tile";
             // maybe destroy the ones existing the position where we wann insert before inserting
         }
-    }
+    }*/
     /* find the lower-left- and the upper-right-most tile  */
-    private void GetMaxCorner(out Vector3 minCorner, out Vector3 maxCorner)
+    /*private void GetMaxCorner(out Vector3 minCorner, out Vector3 maxCorner)
     {
         GameObject[] tiles = GameObject.FindGameObjectsWithTag("Floor tile");
         Vector3 max = Vector3.zero;
@@ -107,41 +230,41 @@ public class LevelGenerator : MonoBehaviour
         maxCorner = max;
         minCorner = min;
 
-    }
+    }*/
 
-    private void PathGenerator(int mainPathCorridorCount, int maxMainPathCorridorLength, int numberOfOffshootBranches, int branchCorridorCount)
+    /*private void PathGenerator(int mainPathCorridorCount, int maxMainPathCorridorLength, int numberOfOffshootBranches, int branchCorridorCount)
     {
         List<int> mainPathCorridors = CorridorLister(mainPathCorridorCount, maxMainPathCorridorLength);
         GameObject theMap = new GameObject("The Map");
         Vector3 maxCorner = Vector3.zero;
         Vector3 minCorner = Vector3.zero;
         Vector3 startPos = Vector3.zero;
-        //Vector3 absoluteStartPos = startPos; // may not be needed
+        //Vector3 absoluteStartPos = startPos; // may not be needed*/
 
-        /* CREATE PATH */
+    /* CREATE PATH */
 
-        while (mainPathCorridors.Count != 0)
-        {
+    /*while (mainPathCorridors.Count != 0)
+    {
 
-            int corridorLength = ListPop(mainPathCorridors);
-            Vector3 corridorDirection = RandomCardinalDirection();
-            LayCorridorTiles(startPos, corridorDirection, corridorLength, theMap, Color.red);
-            startPos += corridorDirection * corridorLength;
-        }
-
-        GetMaxCorner(out minCorner, out maxCorner);
-        LayOuterWall(minCorner, maxCorner);
-
-        //GameObject rand = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        //rand.transform.position = GetRandomRoomCorner(minCorner, maxCorner);
-
-
-
+        int corridorLength = ListPop(mainPathCorridors);
+        Vector3 corridorDirection = RandomCardinalDirection();
+        LayCorridorTiles(startPos, corridorDirection, corridorLength, theMap, Color.red);
+        startPos += corridorDirection * corridorLength;
     }
 
+    GetMaxCorner(out minCorner, out maxCorner);
+    LayOuterWall(minCorner, maxCorner);*/
+
+    //GameObject rand = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    //rand.transform.position = GetRandomRoomCorner(minCorner, maxCorner);
 
 
-    private void LayOuterWall(Vector3 minCorner, Vector3 maxCorner)
+
+
+
+
+
+    /*private void LayOuterWall(Vector3 minCorner, Vector3 maxCorner)
     {
         int offset = 5;
         int verticalLength = (int)Mathf.Abs(minCorner.z - maxCorner.z);
@@ -187,54 +310,10 @@ public class LevelGenerator : MonoBehaviour
     private void RayCastInCardinalDirection(Vector3 objectPosition, Vector3 cardinalDirection)
     {
 
-    }
+    }*/
 
 
 
 
-    /* MAY BE NEEDED */
-    //private Vector3 GetWorldPosition(int x, int z, float cellSize)
-    //{
-    //    return new Vector3(x, 0, z) * cellSize;
-    //}
-
-
-
-
-    private void Start()
-    {
-
-        Noise noise = new Noise();
-
-        //Debug.Log((uint)Mathf.RoundToInt((float)2 / 3));
-
-        Debug.Log(uint.MaxValue);
-
-        for (int i = 0; i < 10000; i++)
-        {
-
-            Debug.Log(noise.ZeroOrOne((uint)i, 36));
-
-            //if(noise.Get1DNoiseZeroToOne((uint)i, 256) >= .5f)
-            //{
-            //Debug.Log(true);
-            //}
-            //else
-            //{
-            // Debug.Log(false);
-            //}
-        }
-
-        
-        PathGenerator(25,10, 0, 0);
-        //GetMaxCorner();
-
-        //for (int i = 0; i < 15; i++)
-        //{
-        //    Debug.Log(i + ". step is: " + NorthOrWest());
-        //}
-
-
-    }
 
 }
--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -6,10 +6,17 @@ namespace NoiseUtils
 {
     public class Noise
     {
-        const uint noise1 = 0b11010111010101010010011111101011;
-        const uint noise2 = 0b11101101101010101100000101101101;
-        const uint noise3 = 0b11000101010100111101000110001011;
+        //const uint noise1 = 0b11010111010101010010011111101011;
+        //const uint noise2 = 0b11101101101010101100000101101101;
+        //const uint noise3 = 0b11000101010100111101000110001011;
 
+        const uint noise1 = 0xB5297A4D;
+        const uint noise2 = 0x68E31DA4;
+        const uint noise3 = 0x1B56C4E9;
+
+        const uint prime = 0b11101110011010110010101101011101;
+
+        /* 1D */
         public uint Get1DNoiseUint (uint position, uint seed)
         {
             uint mangled = position;
@@ -52,6 +59,38 @@ namespace NoiseUtils
             return (uint)Mathf.RoundToInt((float)mangled / uint.MaxValue);
         }
 
+        public int ZeroOrOneOrMinusOne(uint position, uint seed)
+        {
+            uint mangled = position;
+            mangled *= noise1;
+            mangled += seed;
+            mangled ^= (mangled << 8);
+            mangled += noise2;
+            mangled ^= (mangled >> 8);
+            mangled *= noise3;
+            mangled ^= (mangled << 8);
+
+            return (int)(mangled % 3) - 1;
+        }
+
+
+        /* 2D */
+
+        public uint Get2DNoiseUint(uint posX, uint posY, uint seed)
+        {
+            return Get1DNoiseUint(posX + (prime * posY), seed);
+        }
+
+        public float Get2DNoiseZeroToOne(uint posX, uint posY, uint seed)
+        {
+            return Get1DNoiseZeroToOne(posX + (prime * posY), seed);
+        }
+        
+
 
     }
 }
+
+
+
+
